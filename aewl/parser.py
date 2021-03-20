b21ee28c8ef323e019ef6f1dc932de254c73d079
Sigmund Klåpbakken <sigmundklaa@outlook.com>
2021-03-20 15:09:49 +0100

    Parsing works

--- aewl/parser.py
+++ aewl/parser.py
@@ -2,11 +2,11 @@
 from lark import (
     Lark,
     Transformer as BaseTransformer,
-    v_args
+    v_args,
+    Tree
 )
 
-from widget import Widget
-from unit import Unit
+from unit import Unit, Widget
 
 GRAMMAR_FILE = 'aewl/grammar.lark'
 
@@ -18,8 +18,62 @@ class Transformer(BaseTransformer):
     array = list
     number = v_args(inline=True)(float)
 
+
+def parse_value(value, scope):
+    if isinstance(value, Tree):
+        if value.data in ('value_expr', 'mul_expr'):
+            left, op, right = value.children
+            left, right = parse_value(left, scope), parse_value(right, scope)
+
+            m_name = {
+                'value_expr': {
+                    '+': '__add__',
+                    '-': '__sub__'
+                },
+                'mul_expr': {
+                    '*': '__mul__',
+                    '/': '__div__',
+                    '%': '__mod__'
+                }
+            }[value.data][op]
+
+            method = getattr(left, m_name)
+            
+            return method(right)
+        elif value.data == 'widget_def':
+            name, inherits = value.children[:2]
+            widget = scope.make_widget(str(name), inherits)
+
+            parse_widget(widget, value.children[2])
+
+            return widget
+        elif value.data == 'variable':
+            return scope.get_variable(str(value.children[0]))
+        elif value.data == 'property':
+            if not isinstance(scope, Widget):
+                raise Exception('property can not be defined outside widget')
+
+            return scope.get_property(str(value.children[0]))
+    elif isinstance(value, list):
+        value = [parse_value(x, scope) for x in value]
+
+    return value
+
 def parse_widget(widget, tree):
-    pass
+    for t in tree.children:
+        if t.data == 'variable_def':
+            parse_variable(widget, t)
+        elif t.data == 'property_def':
+            name, value = t.children
+
+            widget.add_property(str(name), parse_value(value, widget))
+        else:
+            raise Exception('Unexpected %s' % t.data)
+
+
+def parse_variable(scope, tree):
+    scope.add_variable(
+        str(tree.children[0]), parse_value(tree.children[1], scope))
 
 def parse(src, name):
     l = Lark.open(GRAMMAR_FILE, parser='lalr', transformer=Transformer())
@@ -28,20 +82,21 @@ def parse(src, name):
     unit = Unit(name)
 
     for t in tree.children:
-        if t.data == 'widget_def':
-            widget = unit.add_widget(*t.children[:2])
+        if t.data in ('widget_def', 'display_def'):
+            name, inherits = t.children[:2]
+            widget = unit.add_widget(
+                str(name), inherits, is_display=(t.data == 'display_def'))
 
-            parse_widget(widget, t.children)
+            parse_widget(widget, t.children[2])
         elif t.data == 'variable_def':
-            pass
-        elif t.data == 'display_def':
-            pass
+            parse_variable(unit, t)
         else:
             raise Exception('Unexpected %s' % t.data)
 
     return tree
 
+
 if __name__ == '__main__':
 
     with open('example') as fp:
-        print(parse(fp.read()).pretty())
+        print(parse(fp.read(), '').pretty())
--- aewl/unit.py
+++ aewl/unit.py
@@ -1,16 +1,38 @@
 
-from widget import Widget
+class Scope:
+    def __init__(self, parent=None):
+        self.variables = {}
+        self.parent = parent
 
-class Unit:
+    def add_variable(self, key, value):
+        self.variables[key] = value
+
+    def get_variable(self, key):
+        try:
+            return self.variables[key]
+        except KeyError:
+            if self.parent is not None:
+                return self.parent.get_variable(key)
+            
+            raise
+
+class Unit(Scope):
     def __init__(self, name):
         self.name = name
 
         self.links = []
         self.widgets = {}
-        self.variables = {}
 
-    def add_widget(self, name, inherits_name=None):
-        if inherits_name is not None:
+        super().__init__()
+
+    def add_widget(self, name, *args, **kwargs):
+        widget = self.make_widget(name, *args, **kwargs)
+        self.widgets[name] = widget
+
+        return widget
+
+    def make_widget(self, name, inherits_name=None, is_display=False):
+        if inherits_name is None:
             inherits = self.get_widget(inherits_name)
 
             if inherits is None:
@@ -18,7 +40,9 @@ class Unit:
         else:
             inherits = None
 
-        return Widget(name, inherits, self)
+        type_ = Widget if not is_display else Display
+
+        return type_(name, inherits, self)
 
     def get_widget(self, name):
         if name in self.widgets:
@@ -31,3 +55,31 @@ class Unit:
                 return found
 
         return None
+
+class Widget(Scope):
+    def __init__(self, name, inherits=[], parent=None):
+        self.name = name
+        self.inherits = inherits
+        self.properties = {}
+
+        super().__init__(parent=parent)
+
+    def export(self): pass
+
+    def add_property(self, key, value):
+        self.properties[key] = value
+
+    def get_property(self, key):
+        try:
+            return self.properties[key]
+        except KeyError:
+            if isinstance(self.parent, Widget):
+                return self.parent.get_property(key)
+
+            raise
+
+    def make_widget(self, *args, **kwargs):
+        return self.parent.make_widget(*args, **kwargs)
+
+class Display(Widget):
+    pass
