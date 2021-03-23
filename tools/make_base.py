7ab5b7cebf89ea101fb39ea1d536861933403942
Sigmund Klåpbakken <sigmundklaa@outlook.com>
2021-03-23 18:42:08 +0100

    boy oh boy do i love myself

--- tools/make_base.py
+++ tools/make_base.py
@@ -1,4 +1,6 @@
 
+import json
+from collections import defaultdict
 from armaconfig import loads
 from armaconfig.entry import EOL
 from armaconfig.analyse import Parser
@@ -12,8 +14,23 @@ def get_styles():
         # therefore we dont need to iterate over everything
         # as that needs to be done in a very specific way
         next(parser.parse())
+        out = defaultdict(dict)
 
-        for k, v in parser._scanner.stream.preprocessor.defined.items():
-            print(k, ''.join(v.resolve(parser._scanner.stream)))
+        stream = parser._scanner.stream
+
+        for k, v in stream.preprocessor.defined.items():
+            split = k.split('_')
+            section = split[0].lower()
+
+            out[section]['_'.join(split[1:])] = ''.join(v.resolve(stream)).strip()
+
+        with open('out.githide', 'w') as wp:
+            for section, vs in out.items():
+                wp.write('\n# {}\n'.format(section))
+
+                for k, v in vs.items():
+                    wp.write('{} = {}\n'.format(k, str(v)))
+
+
 
 get_styles()
