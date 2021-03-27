f4d0eacc582280efaba1c0e76c0bd3b0499f999a
Sigmund Klåpbakken <sigmundklaa@outlook.com>
2021-03-27 22:00:20 +0100

    added structuredtext

--- aewl/widgets/text.py
+++ aewl/widgets/text.py
@@ -6,9 +6,21 @@ class Text(Widget):
     raw_name = 'text'
     base_name = 'aewl_text'
 
-    @customizer(Percentage(100), alias='sizeEx')
-    def size(self, k, val):
+    def _resolve_size(self, k, val):
         if isinstance(val, Percentage):
             return self.get_processed('height') * (val / 100)
         else:
             return PixelGrid.pixel_h(val)
+
+    @customizer(Percentage(100), alias='sizeEx')
+    def size(self, k, val):
+        return self._resolve_size(k, val)
+
+class StructuredText(Text):
+    raw_name = 'structured_text'
+    base_name = 'aewl_structuredtext'
+
+    @customizer(Percentage(100), alias='size')
+    def size(self, k, val):
+        # Just changes alias to size instead of sizeEx
+        return self._resolve_size(k, val)
