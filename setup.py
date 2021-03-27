6ea6762398f8ce5cc8672531fc784cec685b9e16
Sigmund Klåpbakken <sigmundklaa@outlook.com>
2021-03-27 22:28:32 +0100

    Added readme to setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,13 @@ from pathlib import Path
 
 path = Path(__file__).parent.absolute()
 
+with open(path.joinpath('README.md')) as fp:
+    long_desc = fp.read()
+
 setup(
     name='aewl',
     description='Extensible GUI creation language for Arma 3',
+    long_description=long_desc,
     author='Sigmund "Sig" Klåpbakken',
     author_email="sigmundklaa@outlook.com",
     url='https://github.com/SigJig/armaconfig.py',
