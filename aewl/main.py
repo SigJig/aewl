cb9e1cc0417a5976cc6a71e657422b9ebce0fb44
Sigmund Klåpbakken <sigmundklaa@outlook.com>
2021-03-27 20:03:05 +0100

    Ability t specify parent

--- aewl/main.py
+++ aewl/main.py
@@ -3,11 +3,14 @@ from pathlib import Path
 from armaconfig import dumps, dump, load
 from .parser import parse
 
-def _export_w_base(src, name):
+def _export_w_base(src, name, parent=None):
     un = parse(src, name)
     un.process_all()
 
-    return un.export(parent=get_base())
+    if parent is None:
+        parent = get_base()
+
+    return un.export(parent=parent)
 
 def get_base():
     with open(Path(__file__).parent.absolute().joinpath('defines.hpp')) as fp:
@@ -19,7 +22,12 @@ def file_to_file(src, *args, **kwargs):
 def str_to_file(src, dst, *args, **kwargs):
     name = kwargs.get('name', '')
 
-    return dump(_export_w_base(src, name), dst, *args, **kwargs)
+    if 'parent' in kwargs:
+        parent = kwargs.pop('parent')
+    else:
+        parent = None
+
+    return dump(_export_w_base(src, name, parent=parent), dst, *args, **kwargs)
 
 def file_to_str(src, *args, **kwargs):
     return str_to_str(src.read(), *args, **kwargs)
@@ -27,4 +35,9 @@ def file_to_str(src, *args, **kwargs):
 def str_to_str(src, *args, **kwargs):
     name = kwargs.get('name', '')
 
-    return dumps(_export_w_base(src, name), *args, **kwargs)
+    if 'parent' in kwargs:
+        parent = kwargs.pop('parent')
+    else:
+        parent = None
+
+    return dumps(_export_w_base(src, name, parent=parent), *args, **kwargs)
