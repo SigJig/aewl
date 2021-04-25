4ce628f3a979447a2b8e1ef53f6f4b2a3f87ded7
Sigmund K <sigmundklaa@outlook.com>
2021-04-25 17:32:41 +0200

    Update main.py

--- aewl/main.py
+++ aewl/main.py
@@ -4,13 +4,10 @@ from armaconfig import dumps, dump, load
 from .parser import parse
 from .context import Context
 
-def _export_w_base(src, name, path_parent=None, parent=None):
+def _export(src, name, path_parent=None, parent=None):
     un = parse(src, name, path_parent)
     ctx = Context(un, parent_export=parent)
     ctx.process_all()
-
-    if parent is None:
-        parent = get_base()
 
     # return un.export(parent=parent)
     return ctx.export
@@ -27,10 +24,6 @@ def _resolve_kwargs(kwargs):
         'parent': _pop_or_none('parent')
     }, kwargs    
 
-def get_base():
-    with open(Path(__file__).parent.absolute().joinpath('data', 'base.hpp')) as fp:
-        return load(fp)
-
 def file_to_file(src, *args, **kwargs):
     kwargs['path_parent'] = Path(src.name).parent.absolute()
 
@@ -40,7 +33,7 @@ def str_to_file(src, dst, *args, **kwargs):
     name = kwargs.get('name', '')
     popped_args, kwargs = _resolve_kwargs(kwargs)
 
-    return dump(_export_w_base(src, name, **popped_args), dst, *args, **kwargs)
+    return dump(_export(src, name, **popped_args), dst, *args, **kwargs)
 
 def file_to_str(src, *args, **kwargs):
     kwargs['path_parent'] = Path(src.name).parent.absolute()
@@ -51,4 +44,4 @@ def str_to_str(src, *args, **kwargs):
     name = kwargs.get('name', '')
     popped_args, kwargs = _resolve_kwargs(kwargs)
 
-    return dumps(_export_w_base(src, name, **popped_args), *args, **kwargs)
+    return dumps(_export(src, name, **popped_args), *args, **kwargs)
